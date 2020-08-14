94c045466c3dd77ffddb380eb75cf613e4a785de
latifrons <latifrons88@gmail.com>
2020-08-14 16:27:01 +0800

    todolist

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/annchain/commongo/pq"
+	"github.com/annchain/commongo/todolist"
+	"time"
 )
 
-func main() {
+func pqTest() {
 	q := &pq.PriorityQueue{}
 	q.Push(&pq.Item{
 		Value:    5,
@@ -24,4 +26,40 @@ func main() {
 	fmt.Println(q.Pop().(*pq.Item).Value)
 	fmt.Println(q.Pop().(*pq.Item).Value)
 	fmt.Println(q.Pop().(*pq.Item).Value)
+}
+
+type Task struct {
+	value int
+}
+
+func (t Task) GetValue() interface{} {
+	return t.value
+}
+
+func (t Task) GetId() string {
+	return fmt.Sprintf("ID-%d", t.value)
+}
+
+func todolistTest() {
+	l := todolist.TodoList{
+		ExpireDuration:          time.Second * 10,
+		MinimumIntervalDuration: time.Second * 5,
+		MaxTryTimes:             3,
+	}
+	l.InitDefault()
+	for i := 0; i < 10; i++ {
+		l.AddTask(Task{
+			value: i,
+		})
+	}
+
+	for {
+		v := l.GetTask()
+		fmt.Println("deque", v)
+		time.Sleep(time.Millisecond * 100)
+	}
+}
+
+func main() {
+	todolistTest()
 }
